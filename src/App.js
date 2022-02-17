4dff8d6b80da14ff01c09e35f1ce8a69916aece9
Vlado <vanovac.vlado@gmail.com>
2022-02-17 11:08:29 +0100

    fetching questions data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import StartScreen from './components/StartScreen/StartScreen';
 import Game from './components/Game/Game';
@@ -6,16 +6,27 @@ import './App.css';
 
 export default function App() {
   const [isGameStarted, setIsGameStarted] = useState(false);
+  const [questionsData, setQuestionsData] = useState([])
+  const [questionsLoaded, setQuestionsLoaded] = useState(false)
 
   const startGame = () => {
     setIsGameStarted((prevIsGameStarted) => !prevIsGameStarted);
   }
 
+  useEffect(() => {
+    if (isGameStarted) {
+      fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple')
+          .then((response) => response.json())
+          .then((data) => setQuestionsData(data?.results))
+          .then(() => setQuestionsLoaded(true))
+    }
+  }, [isGameStarted])
+
   return (
       <div>
         {isGameStarted ?
-            <Game /> :
-            <StartScreen startGame={startGame}/>
+            <Game questionsData={questionsData} questionsLoaded={questionsLoaded} /> :
+            <StartScreen startGame={startGame} />
         }
       </div>
   );
