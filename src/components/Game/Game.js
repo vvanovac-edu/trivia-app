bbca0b1bf1875ad5c0f3822dca9adb6b1e89af27
Vlado <vanovac.vlado@gmail.com>
2022-02-17 11:18:50 +0100

    fetching questions data and passing through Game component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-export default function Game() {
+import Question from '../Question/Question';
+import './Game.css';
+
+export default function Game(props) {
+  const questionElements = props.questionsData.map((questionData, index) => (
+      <Question key={index} questionData={questionData} />
+  ))
+
   return (
-      <h1>Game component</h1>
+      <div className="game-container">
+        {questionElements}
+        {props.questionsLoaded &&
+            <div className="button-wrapper">
+              <button className="check-answers-button">Check Answers</button>
+            </div>
+        }
+      </div>
   )
 }
