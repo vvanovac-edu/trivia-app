3885bb50ddefbadb46747cf6673e971fd8a9e89b
Vlado <vanovac.vlado@gmail.com>
2022-02-18 15:01:58 +0100

    changing text color on StartScreen component

--- src/components/StartScreen/StartScreen.js
+++ src/components/StartScreen/StartScreen.js
@@ -6,8 +6,8 @@ export default function StartScreen(props) {
   return (
       <div className="start-screen-container">
         <div className="start-screen-wrapper">
-          <h1 className="start-screen-title">Welcome to Trivia App</h1>
-          <h3 className="start-screen-description">Play quiz and check your knowledge</h3>
+          <h1 className="start-screen-title">Welcome to Trivia App!</h1>
+          <h3 className="start-screen-description">Play quiz and check your knowledge.</h3>
           <button
               className="start-screen-button"
               onClick={props.startGame}
